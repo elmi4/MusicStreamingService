df8262b42b95c65ca09fe91e4e69a62696173a27
Petros <petrospapa21@gmail.com>
2020-04-09 05:30:17 +0300

    GENERAL: - Restructured the project's packages (change package names in "Edit configuration" in order to run) - Some cleanup of inconsistencies and deletion of unused classes
    SPECIFIC:
    - ConnectionInfo in every constructor
    - Removed some equals() and hashcode() methods since we were not using objects of these classes in Collections
    - Put the ConnectionInfo of 3 brokers (Broker, OtherBroker, OtherBroker2) in BrokerCredentials.txt
    - Removed the totalChunks field of MusicFile, since we probably don't need it.
     The publisher is aware of the total numbers and the other classes just wait for null to finish asking chunks

--- src/eventdelivery/OtherBroker.java
+++
@@ -1,8 +0,0 @@
-package eventdelivery;
-
-public class OtherBroker {
-    public static void main(String args[]){
-        Broker test2 = new Broker("127.0.0.1", 4080);
-        test2.initiate();
-    }
-}
